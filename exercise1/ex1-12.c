5928dad7602c56d7cb36766f4065734aefe80965
devaki <devaki.sidireddy@Technoidentity.com>
2023-06-05 15:30:46 +0530

    feat:  add one word per line

---
+++ exercise1/ex1-12.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main(){
+#define IN 1
+#define OUT 0
+
+        int c, state;
+        state = OUT;
+
+        while ((c = getchar()) != EOF) {
+            if (c == ' ' || c == '\n' || c == '\t') {
+                if(state == IN){
+                    putchar('\n');
+                    state = OUT;
+                }
+            }
+            else if (state == OUT) {
+                state = IN;
+                putchar(c);
+
+            }else
+                putchar(c);
+//            printf("%d %d %d\n", );
+
+        }
+
+    }
