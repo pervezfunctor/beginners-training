635e092e5b8c678c2bb54997cb18af489456456c
Sohaib Hussain <Sohaib7997@users.noreply.github.com>
2023-06-05 16:29:39 +0530

    feat :add ex_1.2

---
+++ chapter-1/ex_1.2.c
@@ -0,0 +1,9 @@
+//
+// Created by technoidentity on 5/6/23.
+//
+#include<stdio.h>
+void main(){
+    printf("hello  world\n");
+    printf("hello \c world\n"); // \c is not an escape character
+    printf("hello \t world");
+}
