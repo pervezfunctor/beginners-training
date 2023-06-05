ede1f110e03e941a16ce44ec90d5cda89ff8aa10
devaki <devaki.sidireddy@Technoidentity.com>
2023-06-05 15:00:18 +0530

    feat:  add verify expression

---
+++ exercise1/ex1.6.c
@@ -0,0 +1,10 @@
+//
+// Created by technoidentity on 31/5/23.
+//
+#include<stdio.h>
+int main(){
+    int ch = getchar();
+    int res = (ch != EOF);
+    printf("the expression getchar() != EOF is : %d\n",res);
+    return 0;
+}
