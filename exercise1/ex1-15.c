1b3d53e453e5c178edd5fc68192cb21acda3f848
devaki <devaki.sidireddy@Technoidentity.com>
2023-06-05 16:47:12 +0530

    feat:  add temp conversion

---
+++ exercise1/ex1-15.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+float Celsius(float fahr);
+ int main()
+ {
+    float fahr;
+    int lower,upper,step;
+    lower =0;
+    upper = 300;
+    step = 20;
+    fahr = lower;
+    while(fahr <= upper){
+        printf("%3.0f %6.1f\n",fahr,Celsius(fahr));
+        fahr = fahr + step;
+
+    }
+}
+float Celsius(float fahr)
+{
+    return(5.0/9.0) * (fahr-32.0);
+}
