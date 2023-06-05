6dc6d4fe111a10d44a99f36987b9697b5ad1a01b
varnika <saivarnika.siripurapu@technoidentity.com>
2023-06-05 14:25:44 +0530

    feat: add program to print the corresponding celsius to fahrenheit table

---
+++ chapter-1/exercise-1.4.c
@@ -0,0 +1,19 @@
+//
+// Created by technoidentity on 3/6/23.
+//program to print the corresponding celsius to fahrenheit table
+int main()
+{
+    float celsius, fahrenheit;
+    int upper,lower,step;
+    upper=300;
+    lower=0;
+    step=10;
+    printf("celsius\tFahrenheit\n");
+    printf("-----------------\n");
+    celsius=lower;
+    while(celsius<=upper){
+        fahrenheit=(celsius*9.0/5.0)+32.0;
+        printf("%6.2lf\t%9.2lf\n",celsius,fahrenheit);
+        celsius+=step;
+    }
+}
