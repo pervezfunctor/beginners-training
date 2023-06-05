93734bffa30f4ba5f87a21cb3002203fd1443756
Zaid <zaid-ti@users.noreply.github.com>
2023-06-05 16:48:51 +0530

    feat: add ex_1.9

---
+++ src/chapter_1/ex_1.9.c
@@ -0,0 +1,16 @@
+//
+// Created by technoidentity on 5/6/23.
+//
+#include <stdio.h>
+int main(){
+    int c,lastc;
+    lastc='a';
+    while((c= getchar()) != EOF){
+        if(c!= ' ') putchar(c);
+        if(c==' ') {
+            if(lastc!=' ')
+                putchar(c);
+         lastc=c;
+        }
+    }
+}
