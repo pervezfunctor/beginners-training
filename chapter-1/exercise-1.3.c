ab53719deaee298aa69572c68ec45d8ab62ad4bf
varnika <saivarnika.siripurapu@technoidentity.com>
2023-06-06 10:56:44 +0530

    fix: add program that prints heading

--- chapter-1/exercise-1.3.c
+++ chapter-1/exercise-1.3.c
@@ -11,7 +11,7 @@ int main(){
     printf("fahrenheit  celsius\n");
     fahrenheit=lower;
     while(fahrenheit<=upper){
-        celsius=(fahrenheit*9.0/5.0)+32.0;
+        celsius=(fahrenheit*9.0/5.0)-32.0;
         printf("%6.2lf\t%9.2lf\n",fahrenheit,celsius);
         fahrenheit+=step;
     }
