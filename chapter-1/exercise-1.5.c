4df9afc4bb60f8ba18474fef83cbe8fb8893b188
varnika <saivarnika.siripurapu@technoidentity.com>
2023-06-06 10:58:32 +0530

    fix: add program to print the table in reverse order

--- chapter-1/exercise-1.5.c
+++ chapter-1/exercise-1.5.c
@@ -5,18 +5,9 @@
 
 #include <stdio.h>
 int main(){
-    float celsius, farenheit;
-    int lower=0;
-    int upper=300;
-    int step=10;
+    int fahrenheit;
+   printf("fahrenheit\tcelsius\n");
+   for(fahrenheit=300;fahrenheit>=0;fahrenheit=fahrenheit-20)
 
-    printf("celsius\tfarenheit\n");
-    printf("----------------\n");
-
-    celsius=upper;
-    while (celsius>=lower){
-        farenheit=(celsius*9.0/5.0)+32.0;
-        printf("%6.2lf\t%9.2lf\n",celsius,farenheit);
-        celsius -= step;
-    }
+       printf("%3d %17.1f\n",fahrenheit,(5.0/9.0)*(fahrenheit-32));
 }
