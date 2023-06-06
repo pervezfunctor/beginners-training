43f1c006cbdb8cd121c74c1b9b5ba1e5217866fb
yamuna <saripilli.yamuna@Technoidentity.com>
2023-06-06 17:45:12 +0530

    feat:add to print all input lines that are longer than 80 characters.

---
+++ Chapter-1/ex-1.17.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#define MAXLINE 1000
+int getLine(char line[], int maxline);
+int main(){
+    int len;
+    char line[MAXLINE];
+    while((len = getLine(line, MAXLINE))>0)
+        if(len>80)
+            printf("line longer than 80 characters:%s", line);
+    return 0;
+}
+int getLine(char line[], int maxline){
+    int i=0;
+    char c;
+    while((c=getchar())!='\n' && c!=EOF){
+        if(i<maxline-1)
+            line[i++]=c;
+    }
+    line[i]='\0';
+    return i;
+}
