45995ace71d2295ce8132bc5f75c8d15029fadc2
pavani <pavani.sadi@Technoidentity.com>
2023-06-06 10:46:31 +0530

    feat:adding replacing \b,\t,\\

---
+++ chapter-1/excercise 1.10.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+    int ch;
+    while ((ch = getchar()) != EOF) {
+        if (ch == '\b')
+            printf("\\b");
+        else if (ch == '\t')
+            printf("\\t");
+        else if (ch == '\\')
+            printf("\\\\");
+        else
+            putchar(ch);
+    }
+
+}
