c9f05fe2b049168fee054707fc14da322d6886ff
devaki <devaki.sidireddy@Technoidentity.com>
2023-06-07 13:08:42 +0530

    feat : add arbitary long input lines

---
+++ exercise1/ex1.16.c
@@ -0,0 +1,50 @@
+
+//program to print the length of arbitrarily long input lines
+#include <stdio.h>
+#define MAXLINE 1000
+/* maximum input line length */
+int get_input_line(char line[], int maxline);
+void copy(char to[], char from[]);/* print the longest input line */
+int main()
+{
+    int len;
+/* current line length */
+    int max;
+/* maximum length seen so far */
+    char line[MAXLINE];
+/* current input line */
+    char longest[MAXLINE]; /* longest line saved here */
+    max = 0;
+    while ((len = get_input_line(line, MAXLINE)) > 0)
+        if (len > max) {
+            max = len;
+            copy(longest, line);
+        }
+    if (max > 0) /* there was a line */
+        printf("longest line is = %s", longest);
+    printf("length of longest line=%d",max);
+
+    return 0;
+}
+/* get_input_line: read a line into s, return length*/
+int get_input_line(char s[],int lim)
+{
+    int c, i;
+
+    for (i=0; i < lim-1 && (c=getchar())!=EOF && c!='\n'; ++i)
+        s[i] = c;
+    if (c == '\n') {
+        s[i] = c;
+        ++i;
+    }
+    s[i] = '\0';
+    return i;
+}
+/* copy: copy 'from' into 'to'; assume to is big enough */
+void copy(char to[], char from[])
+{
+    int i;
+    i = 0;
+    while ((to[i] = from[i]) != '\0')
+        ++i;
+}
