83f9433ef5df4b78a3ab21770fe11143969f05de
varnika <saivarnika.siripurapu@technoidentity.com>
2023-06-09 10:24:49 +0530

    feat:add program horizontal histogram

---
+++ chapter-1/exercise-1.13.c
@@ -0,0 +1,30 @@
+//
+// Created by technoidentity on 5/6/23.
+//
+//horizontal histogram
+#include <stdio.h>
+#define MAX_LENGTH 20
+int main(){
+    int lengths[MAX_LENGTH+1]={0};
+    int c;
+    int length=0;
+    printf("enter input(press cntrl+D to exit):\n");
+    while((c=getchar())!=EOF){
+        if(c==' ' || c=='\n' || c=='\t'){
+            if(length>0 && length<=MAX_LENGTH){
+                lengths[length]++;
+            }
+            length=0;
+        }else{
+            length++;
+        }
+    }
+    for(int i=1;i<=MAX_LENGTH;i++){
+        printf("%2d:",i);
+        for(int j=0;j < lengths[i];j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
