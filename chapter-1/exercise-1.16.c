58b1e0225d82d64dd0baa89c148becb36d64f05e
varnika <saivarnika.siripurapu@technoidentity.com>
2023-06-09 12:45:41 +0530

    fix:add program to print the length of arbitrarily long input lines

--- chapter-1/exercise-1.16.c
+++ chapter-1/exercise-1.16.c
@@ -4,26 +4,22 @@
 //program to print the length of arbitrarily long input lines
 #include <stdio.h>
 #define MAXLINE 1000
-/* maximum input line length */
 int getLine(char line[], int maxline);
-void copy(char to[], char from[]);/* print the longest input line */
+void copy(char to[], char from[]);
 int main()
 {
     int len;
-/* current line length */
     int max;
-/* maximum length seen so far */
     char line[MAXLINE];
-/* current input line */
-    char longest[MAXLINE]; /* longest line saved here */
+    char longest[MAXLINE];
     max = 0;
     while ((len = getLine(line, MAXLINE)) > 0)
         if (len > max) {
             max = len;
             copy(longest, line);
         }
-    if (max > 0) /* there was a line */
-        printf("longest line is = %s", longest);
+    if (max > 0)
+        printf("longest line is = %s\n", longest);
         printf("length of longest line=%d",max);
 
     return 0;
@@ -31,16 +27,15 @@ int main()
 /* getline: read a line into s, return length*/
 int getLine(char s[],int lim)
 {
-int c, i;
-
-for (i=0; i < lim-1 && (c=getchar())!=EOF && c!='\n'; ++i)
-s[i] = c;
-if (c == '\n') {
-s[i] = c;
-++i;
-}
-s[i] = '\0';
-return i;
+    int c, i;
+    for (i=0; i < lim-1 && (c=getchar())!=EOF && c!='\n'; ++i)
+        s[i] = c;
+    if (c == '\n') {
+        s[i] = c;
+        ++i;
+    }
+    s[i] = '\0';
+    return i;
 }
 /* copy: copy 'from' into 'to'; assume to is big enough */
 void copy(char to[], char from[])
