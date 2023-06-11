8e47922f6d1b0c6bb9a459fa05b83913bae06ade
Sai Hemanth Bathula <saihemanthbathula@users.noreply.github.com>
2023-06-11 18:48:31 +0530

    feat: add ex-1.3

---
+++ chapters/chapter-1/ex-1.3.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    float fahr,celsius;
+    float lower,upper,step;
+    lower=0;
+    upper=300;
+    step=20;
+    fahr=lower;
+    printf(" Fahrenheit Celsius \n");
+    while(fahr<=upper){
+        celsius = 5.0/9.0 * (fahr-32) ;
+        printf("%3.0f \t %3.1f \n",fahr,celsius);
+        fahr=fahr+step;
+    }
+
+}
