489b946588a8fd7bcab01dd0fe0341e3a473879c
Sai Hemanth Bathula <saihemanthbathula@users.noreply.github.com>
2023-06-11 18:49:14 +0530

    feat: add ex-1.8

---
+++ chapters/chapter-1/ex-1.8.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+    int ch, nb=0,nt=0,nl=0;
+    while((ch = getchar()) != EOF){
+        if(ch == ' ')
+            nb++;
+        else if(ch == '\t')
+            nt++;
+        else if(ch == '\n')
+            nl++;
+    }
+    printf("no of blanks %d \n",nb);
+    printf("no of tabs %d \n ",nt);
+    printf("no of lines %d \n",nl);
+    return 0;
+}
