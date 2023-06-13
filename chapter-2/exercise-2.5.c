8cf7c1acd74db2e514961b2302d394004eedcba9
varnika <saivarnika.siripurapu@technoidentity.com>
2023-06-13 21:23:09 +0530

    fix:add function any(s1,s2) which returns the first location in the string

---
+++ chapter-2/exercise-2.5.c
@@ -0,0 +1,36 @@
+//
+// Created by technoidentity on 8/6/23.
+//
+//function any(s1,s2) which returns the first location in the string
+#include <stdio.h>
+#include <string.h>
+int any(char *s1,char *s2)
+{
+    int i, j;
+    for(i = 0; i < strlen(s1); i++) {
+        for(j = 0; j < strlen(s2); j++) {
+            if(s1[i] == s2[j])
+                break;
+        }
+        if(j != strlen(s2))
+            break;
+    }
+    if(i == strlen(s1))
+        return -1;
+    else
+        return i;
+}
+int main()
+{
+    char *s1[100];
+    char *s2[100];
+    printf("enter the string s1:");
+    scanf("%s",s1);
+    printf("enter the string s2:");
+    scanf("%s",s2);
+    int idx = any(s1,s2);
+    if(idx != -1) {
+        printf("First character matching  is at %d\n", idx);
+    }
+    return 0;
+}
