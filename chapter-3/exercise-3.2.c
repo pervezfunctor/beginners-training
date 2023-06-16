70f126068f8f3b1b9fc0a868c13395f094e32d3a
varnika <saivarnika.siripurapu@technoidentity.com>
2023-06-16 16:17:24 +0530

    feat:add function for converting escape sequences into the real characters

---
+++ chapter-3/exercise-3.2.c
@@ -0,0 +1,38 @@
+//
+// Created by technoidentity on 13/6/23.
+//
+//converting escape sequences into the real characters.
+#include <stdio.h>
+void escape(char s[], const char t[]) {
+    int i, j;
+    for(i = 0, j = 0; t[i] != '\0';i++, j++)
+    {
+        if(t[i] == '\\'){
+            switch(t[++i]){
+                case 'n':
+                    s[j] = '\n';
+                    break;
+                case 't':
+                    s[j] = '\t';
+                    break;
+                default:
+                    s[j++] = '\\';
+                    s[j] = t[i];
+                    break;
+            }
+        }else{
+          s[j] = t[i];
+        }
+
+    }
+    s[j]= '\0';
+}
+int main(){
+    const char input[100];
+    printf("enter the string:");
+    scanf("%s",input);
+    char output[100];
+    escape(output,input);
+    printf("visible escape sequences :%s\n",output);
+    return 0;
+}
