c771358ec4fe2102cd41814d8f2eb3576c26f819
varnika <saivarnika.siripurapu@technoidentity.com>
2023-06-20 16:54:49 +0530

    feat:add function that copies the string t to the end of s

--- chapter-5/exercise-5.3.c
+++ chapter-5/exercise-5.3.c
@@ -7,9 +7,9 @@ void strcat(char *s,char *t);
 int main(){
     char s[100];
     char t[100];
-    printf("enter string1:%s",s);
+    printf("enter string1:");
     scanf("%s",s);
-    printf("enter string2:%s",t);
+    printf("enter string2:");
     scanf("%s",t);
     strcat(s,t);
     printf("concatenated string:%s",s);
