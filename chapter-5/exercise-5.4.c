c3b35e1de0dcadf7332b4d207ee30f46086d8b6c
varnika <saivarnika.siripurapu@technoidentity.com>
2023-06-20 16:00:55 +0530

    feat:add the function strend(s,t).

---
+++ chapter-5/exercise-5.4.c
@@ -0,0 +1,33 @@
+//
+// Created by technoidentity on 20/6/23.
+//
+//the function strend(s,t), which returns 1 if the string t occurs at the end of the string s, and zero otherwise
+#include <stdio.h>
+#include <string.h>
+int strend(char *s,char *t);
+int main(){
+    char s[100];
+    char t[100];
+    printf("enter string1:%s",s);
+    scanf("%s",s);
+    printf("enter string2:%s",t);
+    scanf("%s",t);
+    strend(s,t);
+    printf("%d", strend(s,t));
+    return 0;
+}
+int strend(char *s,char *t){
+    char *ss=s;
+    char *ts=t;
+    while(*ss){
+        t=ts;
+        for(s=ss;*s==*t;s++,t++){
+            if(*s=='\0'&& *t=='\0'){
+                return 1;
+            }
+        }
+        ss++;
+    }
+    return 0;
+
+}
