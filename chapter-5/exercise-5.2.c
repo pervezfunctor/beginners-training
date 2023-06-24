87c1c60d234cf18f2465a798f2983578c75b2242
varnika <saivarnika.siripurapu@technoidentity.com>
2023-06-24 22:26:23 +0530

    feat:add the floating-point analog of getint

---
+++ chapter-5/exercise-5.2.c
@@ -0,0 +1,81 @@
+//
+// Created by technoidentity on 20/6/23.
+//
+//the floating-point analog of getint
+#include <stdio.h>
+#include <ctype.h>
+#define SIZE 100
+
+int getch(void);
+void ungetch(int c);
+int getfloat(float *pn);
+
+int main() {
+    float n;
+    int result;
+    result = getfloat(&n);
+
+    if (result != 0) {
+        printf("Valid floating-point representation: %f\n", n);
+    } else {
+        printf("Invalid representation\n");
+    }
+
+    return 0;
+}
+
+char buffer[SIZE];
+int bufp = 0;
+
+int getch(void) {
+    return (bufp > 0) ? buffer[--bufp] : getchar();
+}
+
+void ungetch(int c) {
+    if (bufp >= SIZE) {
+        printf("Error: Buffer is full\n");
+    } else {
+        buffer[bufp++] = c;
+    }
+}
+
+int getfloat(float *pn) {
+    int c, sign;
+    float pow=1.0;
+
+    while (isspace(c = getch()));
+
+    if (!isdigit(c) && c != EOF && c != '+' && c != '-') {
+        ungetch(c); // Push back non-digit character
+        return 0;
+    }
+
+    sign = (c == '-') ? -1 : 1;
+
+    if (c == '+' || c == '-') {
+        int next = getch();
+        if (!isdigit(next)) {
+            ungetch(next); // Push back non-digit character
+            ungetch(c); // Push back '+' or '-' character
+            return 0;
+        } else {
+            c = next;
+        }
+    }
+
+    for (*pn = 0.0; isdigit(c );c= getch()) {
+        *pn = 10.0 * *pn + (c - '0');
+    }
+    if(c=='.'){
+        for(pow=1.0;isdigit(c=getch());pow *=10.0){
+            *pn = 10.0 * *pn + (c-'0');
+        }
+    }
+    *pn *= sign/pow;
+
+    if (c != EOF) {
+        ungetch(c);
+    }
+
+    return c;
+}
