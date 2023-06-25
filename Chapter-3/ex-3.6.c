dc83f3484d9c7bf58bd559711462acc70557356d
yamuna <saripilli.yamuna@Technoidentity.com>
2023-06-25 23:34:42 +0530

    feat:add itoa that accepts three arguments instead of two,the third argument is width.

---
+++ Chapter-3/ex-3.6.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+void reverse(char str[],int length){
+    int start=0;
+    int end=length-1;
+    while(start<end) {
+        char temp = str[start];
+        str[start] = str[end];
+        str[end] = temp;
+        start++;
+        end--;
+    }
+}
+void itoa(int n,char str[], int width){
+    int isNegative=0;
+    int i=0;
+    if(n==0){
+        str[i++]='0';
+        str[i]='\0';
+        return;
+    }
+    if(n<0){
+        isNegative=1;
+        n=-n;
+    }
+    while(n!=0){
+        int rem=n%10;
+        str[i++]=rem+'0';
+        n=n/10;
+    }
+    if(isNegative){
+        str[i++]='-';
+    }
+    while(i<width){
+        str[i++]= ' ';
+    }
+    str[i]='\0';
+    reverse(str,i);
+}
+int main(){
+    int n,width;
+    printf("enter the number:");
+    scanf("%d",&n);
+    char str[100];
+    printf("enter the width:");
+    scanf("%d",&width);
+    itoa(n,str,width);
+    printf("padded string:%s\n",str);
+    return 0;
+
+}
