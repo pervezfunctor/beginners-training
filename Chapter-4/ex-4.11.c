4c9933f7ee1746d7e40453e17f2aa1f87d78c16b
yamuna <saripilli.yamuna@Technoidentity.com>
2023-06-25 23:21:40 +0530

    feat:add Modify getop so that it doesn't need to use ungetch using static.

---
+++ Chapter-4/ex-4.11.c
@@ -0,0 +1,117 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAXOP 100
+#define NUMBER '0'
+int getop(char []);
+void push(double);
+double pop(void);
+int main()
+{
+    int type;
+    double op2;
+    char s[MAXOP];
+    while ((type = getop(s)) != EOF) {
+        switch (type) {
+            case NUMBER:
+                push(atof(s));
+                break;
+            case '+':
+                push(pop() + pop());
+                break;
+            case '*':
+                push(pop() * pop());
+                break;
+            case '-':
+                op2 = pop();
+                (pop() - op2);
+                break;
+            case '/':
+                op2 = pop();
+                if (op2 != 0.0)
+                    push(pop() / op2);
+                else
+                    printf("error: zero divisor\n");
+                break;
+            case '%'  :
+                op2 = pop();
+                if (op2 != 0.0)
+                    push((int)pop() % (int)op2);
+                else
+                    printf("error:zero division error\n");
+                break;
+            case '\n':
+                printf("\t%.8g\n", pop());break;
+            default:
+                printf("error: unknown command %s\n", s);
+                break;
+        }
+    }
+    return 0;
+}
+/*push and pop operations on stack*/
+#define MAXVAL 100
+int sp = 0;
+double val[MAXVAL];
+void push(double f)
+{
+    if (sp < MAXVAL)
+        val[sp++] = f;
+    else
+        printf("error: stack full, can't push %g\n", f);
+}
+double pop(void)
+{
+    if (sp > 0)
+        return val[--sp];
+    else {
+        printf("error: stack empty\n");
+        return 0.0;
+    }
+}
+/* getop: get next character or numeric operand */
+#include<ctype.h>
+int getch(void);
+int getop(char s[])
+{
+    int i;
+    static int c = ' ';
+
+    while (c == ' '|| c =='\t')
+        c = getch();
+    s[0] = c; //
+    s[1] = '\0';
+    if (!isdigit(c) && c != '.' && c != '-')
+    {
+        int toReturn = c;
+        c = ' ';
+        return toReturn;
+    }
+    i = 0;
+    if (c == '-' || isdigit(c)) //
+        while (isdigit(s[++i] = c = getch()))
+            ;
+    if (c == '.')
+        while (isdigit(s[++i] = c = getch()))
+            ;
+    s[i] = '\0';
+    if (i == 1 && s[0] == '-')
+        return '-';
+    return NUMBER;
+}
+/*buffer size to store the array of characters*/
+#define BUFSIZE 100
+char buf[BUFSIZE];
+int bufp = 0;
+int getch(void)
+{
+    return (bufp > 0) ? buf[--bufp] : getchar();
+}
+/*void ungetch(int c)
+
+{
+    if (bufp >= BUFSIZE)
+        printf("ungetch: too many characters\n");
+    else
+        buf[bufp++] = c;
+}*/
+
