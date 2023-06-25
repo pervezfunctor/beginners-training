3697b681796b30f1c15047cc8d60130594f35e8d
yamuna <saripilli.yamuna@Technoidentity.com>
2023-06-25 23:32:59 +0530

    feat:add itob(n,s,16) formats s as a hexadecimal integer in s.

---
+++ Chapter-3/ex-3.5.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+void itob(int n,char s[],int b){
+    int i,sign;
+    if((sign=n)<0)
+        n=-n;
+    i=0;
+    do{
+        int remainder =n%b;
+        s[i++]=(remainder<10)? remainder+ '0':remainder+'A'-10;
+    }while((n/=b)>0);
+    if(sign<0)
+        s[i++]='-';
+    s[i]='\0';
+    int start=0;
+    int end=i-1;
+    while(start<end){
+        char temp=s[start];
+        s[start]=s[end];
+        s[end]=temp;
+        start++;
+        end--;
+    }
+}
+int main(){
+    int n;
+    printf("enter the number:");
+    scanf("%d",&n);
+    char str[20];
+    int base=16;
+    itob(n,str,base);
+    printf("%s\n",str);
+    return 0;
+}
