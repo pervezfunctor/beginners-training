85744d2fcd26b3de12e936cc54a2e1dc479456ef
yamuna <saripilli.yamuna@Technoidentity.com>
2023-06-25 23:44:23 +0530

    feat:add escape(s,that converts characters like newline and tab into  visible escape sequences like \n and \t

---
+++ Chapter-3/ex-3.2.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+void escape(char s[], const char t[]) {
+    int i, j;
+    for(i = 0, j = 0; t[i] != '\0';i++, j++)
+    {
+        if(t[i] == '\\'){
+            switch(t[++i]){
+                case 'n':
+                    s[j] = '\n';
+                    break;
+                case 't':
+                    s[j] = '\t';
+                    break;
+                default:
+                    s[j++] = '\\';
+                    s[j] = t[i];
+                    break;
+            }
+        }else{
+            s[j] = t[i];
+        }
+    }
+    s[j]= '\0';
+}
+int main(){
+    const char input[100];
+    printf("enter the string:");
+    scanf("%s",input);
+    char output[100];
+    escape(output,input);
+    printf("visible escape sequences :%s\n",output);
+    return 0;
+}
