04b5da508fffafa941866f9e431af56866da2cf5
pavani <pavani.sadi@Technoidentity.com>
2023-06-26 11:22:02 +0530

    feat:add strcat function

---
+++ chapter-5/exercise-5.3.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int str_cat(char *s, char *t) {
+    while ((*s) != '\0'){
+        s++;
+    }
+    while ((*s = *t) != '\0'){
+        s++;
+        t++;
+    }
+    return 0;
+}
+
+int main(){
+    char s[100];
+    char t[100];
+    printf("enter string1:");
+    scanf("%s",s);
+    printf("enter string2:");
+    scanf("%s",t);
+    str_cat(s,t);
+    printf("concat string:%s", s);
+    return 0;
+}
