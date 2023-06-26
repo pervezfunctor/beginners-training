23cbf2cb0dabedb5b0e974b033130a5eef69fd2b
ramya <paladugula.umasriramya@technoidentity.com>
2023-06-26 11:21:52 +0530

    fix:Add strcat function.

---
+++ chapter5/exercise-5.3.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int str_cat(char *s, char *t) {
+    while ((*s) != '\0'){
+        s++;
+    }
+    while ((*s = *t) != '\0'){
+        s++;
+        t++;
+    }
+    return 0;
+}
+int main() {
+    char s[100];
+    char t[100];
+    printf("string1:");
+    scanf("%s", s);
+    printf("string2:");
+    scanf("%s", t);
+    str_cat(s, t);
+    printf("concat string:%s", s);
+    return 0;
+}
