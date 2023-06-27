498439a2dce0f8d693a2372ce4734a4aea85310a
Sohaib Hussain <Sohaib7997@users.noreply.github.com>
2023-06-27 16:22:00 +0530

    feat:add beginners-assesment

---
+++ beginners-assesment/q7.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <math.h>
+
+int factorial(int n);
+double expression(int x,int n );
+
+int main(){
+    printf("%lf", expression(1,2));
+}
+
+double expression(int x,int n){
+    int i = 0;
+    double sum=0;
+    while(i <= n){
+        sum=sum+(pow(x,i)/ factorial(i)) ;
+        i++;
+    }
+    return sum;
+}
+
+int factorial(int n) {
+    if(n == 0) return 1;
+
+    int fact = 1;
+
+    for(int i = 1; i <= n; i++) {
+        fact = fact * i;
+    }
+
+    return fact;
+}
