ba24e95c82b5c8ca1c8a4e16553c8af1df16f1b8
varnika <saivarnika.siripurapu@technoidentity.com>
2023-06-27 14:03:46 +0530

    feat:add beginners assessment

--- beginner's-assessment/beginners-assessment.c
+++ beginner's-assessment/beginners-assessment.c
@@ -20,28 +20,25 @@ the type of ptr is pointer to int ,it holds the address of the first element, pt
 
 //3
 /*#include <stdio.h>
-void reverse(int s[],int len){
-    int start=0;
-    int end = len-1;
-    while(start<end){
-        int temp=s[start];
-        s[start]=s[end];
-        s[end]=temp;
-        start ++;
-        end --;
-    }
-
+#include <string.h>
+void reverse(char *s,int start,int end){
+    if(start>=end)
+        return;
+    char temp = s[start];
+    s[start]=s[end];
+    s[end]=temp;
+    reverse(s,start+1,end-1);
 }
 int main(){
-    int s[]={1,2,3,4,5};
-    int len=sizeof (s)/sizeof(s[0]);
-    reverse(s,len);
-    printf("reversed array:");
-    for(int i=0;i<len;i++){
-        printf("%d ",s[i]);
-    }
+    char str[100];
+    int length=strlen(str);
+    printf("enter the string:");
+    scanf("%s",&str);
+    reverse(str,0,strlen(str)-1);
+    printf("after reverse:%s\n",str);
     return 0;
-}*/
+}
+*/
 
 //4
 /*function which copies src array to dest array.
